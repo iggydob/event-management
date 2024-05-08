f03618ccf170d63d69bf187b480bedb2eda6f8e3
iggydob <138367360+iggydob@users.noreply.github.com>
2024-05-08 16:44:18 +0300

    Customer module created

---
+++ customer/src/main/java/com/eventmanagement/customer/Customer.java
@@ -0,0 +1,28 @@
+package com.eventmanagement.customer;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
